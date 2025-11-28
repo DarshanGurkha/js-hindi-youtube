25ff641f573392cac930caa5a4fc8b9c2507cc15
Darshan Gurkha <darshangurkha06@gmail.com>
2025-11-28 06:26:15 +0000

    dat

--- 01_basics/03_dataconversion.js
+++ 01_basics/03_dataconversion.js
@@ -17,3 +17,26 @@ console.log(BooleanIsLoggedIn)
 //0 => false  &  1=>true
 // "darshan"  => true
 //  " " => false
+//operations // 
+
+let value= 1;
+let negvalue= -value;
+console.log(negvalue);
+
+//types of oeprators : +,-,*,/,**,% //
+console.log(5+6);
+console.log (1 + "2");
+console.log (1 + 2 + "2");
+console.log (1 + "2" + 2);
+console.log ( "1" + 2 + "2");
+console.log ( "1" + 2 + 2);
+
+console.log(+true);
+let num1,num2,num3
+
+num1=num2=num3= 2+2;
+
+
+let gamecounter=100;
+++gamecounter;
+console.log(gamecounter)
