e1f9f04ce6238438fefaa8edd437c219b1cdd278
Darshan Gurkha <darshangurkha06@gmail.com>
2025-11-28 03:51:26 +0000

    data conversion

---
+++ 01_basics/03_dataconversion.js
@@ -0,0 +1,19 @@
+/*let score= "darshan"
+console.log(typeof score)
+console.log(typeof (score)); */
+
+/*let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)*/
+
+// 25 => "25"
+// true => 1 and false =>1
+// "25abc" => Nan(Not a Number)
+
+let isLoggedIn= "darshan"
+let BooleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn)
+
+//0 => false  &  1=>true
+// "darshan"  => true
+//  " " => false
