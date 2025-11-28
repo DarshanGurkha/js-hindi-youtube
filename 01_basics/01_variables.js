2e9061acc1de6d8d1b51a498f658eda0a21c1fc0
Darshan Gurkha <darshangurkha06@gmail.com>
2025-11-28 02:59:18 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 123;
+let accountName = "darshan";
+var accountPassword = "2325";
+
+//accountId=2    this is not allow //
+// never use var because of issue is block scope and functonal scope //
+console.table({
+  accountId,
+  accountName,
+  accountPassword
+});
+
