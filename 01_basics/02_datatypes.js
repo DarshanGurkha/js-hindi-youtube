6a143e5525e262babd631d9ff165281a052e9d12
Darshan Gurkha <darshangurkha06@gmail.com>
2025-11-28 03:21:46 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  //treal all js newest version
+ 
+//alert(3+3)  we are using node, not browser
+
+console.log(3
+    +3);                        //prefer code readablity//
+console.log("darshan");
+ 
+let name="dhruv"
+let age= 19
+let IsLoggedIn= false
+
+// number => 2 to power 53
+//bigint  => in graph, stock market website
+//string => ""  or ''
+//boolean => true/false
+//onject 
+//undefined
+//null
+//symbol =>unique
+
+console.log ( typeof undefined);//undefined
+console.log(typeof null); //object
