f726fa3b5ed09ea73ba3de6847fbde3581cb2805
Darshan Gurkha <darshangurkha06@gmail.com>
2025-11-28 10:52:48 +0000

    summarydatatypes

---
+++ 01_basics/05_datatypessummary.js
@@ -0,0 +1,37 @@
+//There are two types of datatypes in the javascript
+// 1. primitive
+// 2. Non-primitive
+
+//Primitive data types(called by value)
+// there are 7 types of primitive data types:
+// String,number,boolean, null,undefined,Symbol,bigint
+
+const score= 100
+const scores= 10.3
+
+const isLoggedIn=false
+let username;
+let value=null
+
+const bigNumber=455454545474547874454n
+const id = Symbol('123')
+const anotherId= Symbol('123')
+console.log(id === anotherId)
+
+//Non-Primitive (called by reference)
+// Arrays,objects,Functions
+
+const heroes = ["soldier","police","navy","airforce"];
+
+
+let myObj = {
+                 name: "darshan",
+                  age: 19
+
+        }
+         
+const myfunction= function() {
+console.log("hello world");  
+}
+ console.log(typeof id);
+
