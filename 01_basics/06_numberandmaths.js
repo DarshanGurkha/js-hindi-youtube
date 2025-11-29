589c5c062f41f3408f1ebccde6e082302e756744
Darshan Gurkha <darshangurkha06@gmail.com>
2025-11-29 06:22:39 +0000

    numberandmaths

---
+++ 01_basics/06_numberandmaths.js
@@ -0,0 +1,38 @@
+const score= 500
+//console.log(score);       //direct output
+
+const balance= new Number(200)
+//console.log(balance);                    //new number created
+//console.log(balance.toString().length);   //3
+//console.log(balance.toFixed(2));         //200.00
+
+const anotherNumber = 2125.5
+//console.log(anotherNumber.toPrecision(4));    //2126
+
+const hundreds= 1000000
+//console.log(hundreds.toLocaleString('en-IN'));    //10,00,000
+
+/////////////    maths   //////////////
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(5, 2, 7, 10));
+// console.log(Math.max(5, 4, 7, 10));
+ 
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1))  + min );
+
+
+
+
+
+
+
+
