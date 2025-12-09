ecedced0db10a5f65bf7f1ed70a70061178fa145
Darshan Gurkha <darshangurkha06@gmail.com>
2025-12-09 06:31:51 +0000

    while&dowhile

---
+++ 05_iteration/two.js
@@ -0,0 +1,24 @@
+// // WHILE 
+
+// let index = 0
+// while (index <= 10) {
+//      console.log(`the value of index is :  ${index}`);
+//      index = index + 2   
+// }
+
+// // ARRAY IN WHILE 
+
+// let  myarray = ["darshan", "shivu", "chintu", "chinki"]
+// let arr = 0
+//  while (arr <myarray.length) {
+//       console.log(`the value is ${myarray[arr]}`);
+//        arr = arr + 1
+// }
+
+// DO WHILE   : In this, first it will do anf then condition is checked
+
+let score = 11
+do {
+    console.log(`the value is ${score}`);
+    score ++
+} while (score<=10);
