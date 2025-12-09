d60829c8b46f0b24ccc2af2babc92f02789e698a
Darshan Gurkha <darshangurkha06@gmail.com>
2025-12-09 03:53:02 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,48 @@
+// switch (key) {
+//     case value:
+                                 // Switch Syntax
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=3
+
+switch (month) {
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april")
+        break;
+      default:
+        console.log("default exxecuted")
+        break;
+}
+
+// can also done  by string value
+const monthname= "march"
+switch (monthname) {
+    case "jan":
+        console.log("january")
+        break;
+    case "feb":
+        console.log("feb")
+        break;
+    case "march":
+        console.log("march")
+        break;
+    case "april":
+        console.log("april")
+        break;
+      default:
+        console.log("default executed")
+        break;
+}
